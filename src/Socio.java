57d9e2441e3b852117cca9a3100a730534b79ea5
Alejandro Muriel <amuripra@myuax.com>
2026-02-23 12:39:41 +0100

    Clase Socio creada

---
+++ src/Socio.java
@@ -0,0 +1,27 @@
+public class Socio {
+
+    private final int numeroSocio;
+    private final String nombre;
+
+    /** CONSTRUCTOR:
+     * @param numeroSocio
+     * @param nombre
+     */
+    public Socio(int numeroSocio, String nombre) {
+        this.numeroSocio = numeroSocio;
+        this.nombre = nombre;
+    }
+
+    //Gets
+    public int getNumeroSocio() {
+        return numeroSocio;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
