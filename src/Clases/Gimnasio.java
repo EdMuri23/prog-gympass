6920fa14fd5c412e7c1918d6039ba0cf64d6acbf
Alejandro Muriel <amuripra@myuax.com>
2026-02-24 09:46:08 +0100

    Metodo registrarSocio creado

--- src/Clases/Gimnasio.java
+++ src/Clases/Gimnasio.java
@@ -4,9 +4,14 @@ public class Gimnasio {
 
     private final int codigo;
     private final String nombre;
-    final Socio [] listaSocio;
-    final Sala [] listaSala;
-    int responsableId;
+    private final Socio [] listaSocios;
+    private final Sala [] listaSalas;
+    private int responsableId;
+
+    //Maximos para los array
+    private final int MAX_SOCIOS = 35;
+    private final int MAX_SALAS = 8;
+
 
     /** CONSTRUCTOR:
      * @param codigo
@@ -15,9 +20,70 @@ public class Gimnasio {
     public Gimnasio(int codigo, String nombre) {
         this.codigo = codigo;
         this.nombre = nombre;
-        this.listaSocio = new Socio[35];
-        this.listaSala = new Sala[8];
+        this.listaSocios = new Socio[MAX_SOCIOS];
+        this.listaSalas = new Sala[MAX_SALAS];
         this.responsableId = -1;
+    }
+
+    //Metodos Generales
+
+    /** REGISTRAR_SOCIO:
+     * Registra el socio en el array
+     * @param s
+     * @return
+     */
+    private boolean registrarSocio(Socio s) {
+        boolean registrado = false;
+        int posicion;
+
+        //1º Llamamos a socioEsxisteEnGym para comprobar si el Socio ya esta registrado en el Gimnasio
+        if (socioEsxisteEnGym(s.getNumeroSocio())){
+            //Si no existe buscamos el primer hueco vacio del array listasSocios
+            posicion = buscarPrimerHuecoLibreSocio();
+            if (posicion >= 0 && posicion < MAX_SOCIOS) {
+                listaSocios[posicion] = s;
+                registrado = true;
+            }
+        }
+
+        return registrado;
+    }
+
+    /** BUSCAR_PRIMER_HUECO_LIBRE_SOCIO
+     *
+     * @return
+     */
+    private int buscarPrimerHuecoLibreSocio () {
+        boolean huecoEncontrado = false;
+        int i = 0;
+
+        while (i < MAX_SOCIOS && !huecoEncontrado) {
+            if (listaSocios[i] == null) {
+                huecoEncontrado = true;
+            } else {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    /** SOCIO_EXISTE_EN_GYM
+     *
+     * @param numSocio
+     * @return
+     */
+    private boolean socioEsxisteEnGym(int numSocio) {
+        boolean socioEncontrado = false;
+        int i = 0;
+
+        while (i < MAX_SOCIOS && !socioEncontrado) {
+            if (listaSocios[i].getNumeroSocio() == numSocio) {
+                socioEncontrado = true;
+            }
+            i++;
+        }
+
+        return socioEncontrado;
     }
 
     //Gets
